b497d8ef89aafca18ff41972bbdd429f5e6c47c7
MishaKohanych <mishakokhanych@gmail.com>
2018-08-09 16:25:00 +0300

    left form filter: start

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 new Vue({
     el: "#page",
     data: {
-        cars: cars, //для локальной версии
+        cars: cars,
+        sPrice: {"max": 0, "min": 0},
         baskets: [
         {
             "id": 1,
@@ -20,7 +21,7 @@ new Vue({
         //     });
         // },
         makes: function() {
-            const makes =[]
+            const makes =[];
             for(var i in cars) {
                 if (makes.indexOf(cars[i].make) === -1) {
                     makes.push(cars[i].make);
@@ -29,10 +30,22 @@ new Vue({
             return makes
         },
         filterCars: function() {
-            var self = this
+            var self = this;
             return this.cars.filter(function (car) {
                 return car.make.toUpperCase().indexOf(self.search.toUpperCase()) > -1 || car.model.toUpperCase().indexOf(self.search.toUpperCase()) > -1
             })
+        },
+        valMaxMini: function() {
+            for(var i in cars) {
+                if (cars[i].price > this.sPrice.max) {
+                    this.sPrice.max = cars[i].price;
+                }
+                if (cars[i].price < this.sPrice.max) {
+                    this.sPrice.min = cars[i].price;
+                }
+            }
+
+            return this.sPrice
         },
     },
 });
